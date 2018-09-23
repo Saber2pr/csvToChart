74d5788c9715435ee6a2ca3e8d70752f0bfdb3c4
saber2pr <saber2pr@gmail.com>
2018-09-23 12:00:14 +0800

    toEs6

--- js/csvToChart.js
+++ js/csvToChart.js
@@ -10,36 +10,14 @@
  * @example
  * ```csv
  *  (csv格式)
-    data1,data2
-    1,15
-    6,12
-    3,13
-    4,14
-    5,15
-    6,16
+ *  data1,data2
+ *  1,15
+ *  6,12
+ *  3,13
+ *  4,14
+ *  5,15
+ *  6,16
  * ```
- * ```js
- *  //1. AMD规范，请求模块csvToChart实例
-    require(["csvToChart"], function(csvToChart){
-        //初始化，绑定上传按钮和图表id
-        csvToChart.initData("fileInput", "chart")
-        //绑定图表、对应按钮和图表类型
-        csvToChart.configBtn("chart", "aBtn", "donut")
-        csvToChart.configBtn("chart", "bBtn", "bar")
-    })
-    //2. 直接加载文本
-    var varCsv = 
-    csvToChart.heredoc(function(fn){
-        data1,data2
-        892 ,1072 
-        942 ,1389 
-        945 ,963 
-        955 ,913 
-        825 ,857 
-        852 ,743
-    }
-    csvToChart.createChartUseVar("chart", varCsv)
-   ```     
  * types:
  * (可用类型)
  *  ""
@@ -54,77 +32,87 @@
  *  "pie"
  *  "gauge"
  */
-define(["c3"], function(c3){
-    var _result = null
-    function _set(result){
-        _result = result
-    }
-    function _get(){
-        return _result
-    }
-    function _parseString(string){
-        if(typeof(string)!=="undefined"){
-            var json = string.split("\n")
+import d3 from './lib/d3-5.4.0.min.js'
+import c3 from './lib/c3.min.js'
+
+var _result = null
+
+function _set(result){
+    _result = result
+}
+
+function _get(){
+    return _result
+}
+
+function _parseString(string){
+    if(typeof(string)!=="undefined"){
+        var json = string.split("\n")
             json.pop()
             var array = []
             for(var ele of json){
                 array.push(ele.split(","))
             }
-            return array
-        }else{
-            return
+        return array
+    }else{
+        return
+    }
+}
+
+function _draw(chartId, data, type){
+    c3.generate({
+        bindto: "#"+chartId,
+        data:{
+            rows:data,
+            type:type
         }
+    })
+}
+
+function _createOnEvent(type, chartId){
+    if(_get()!==null){
+        _draw(chartId, _get(), type)
+    }else{
+        return
     }
-    function _draw(chartId, data, type){
-        c3.generate({
-            bindto: "#"+chartId,
-            data:{
-                rows:data,
-                type:type
-            }
-        })
-    }
-    function _createOnEvent(type, chartId){
-        if(_get()!==null){
-            _draw(chartId, _get(), type)
-        }else{
-            return
-        }
-    }
-    function _init(target, chartId, type){
-        var reader = new FileReader()
+}
+
+function _init(target, chartId, type){
+    var reader = new FileReader()
         if(typeof(target)!=="undefined"){
             reader.readAsText(target.files[0]);
             reader.onload = function () {
                 _set(_parseString(this.result))
-                _createOnEvent(type, chartId)
+                    _createOnEvent(type, chartId)
             }
         }else{
             return
         }
-    }
-    function _heredoc(fn){
-        return fn.toString().split('\n').slice(1,-1).join('\n') + '\n'
-    }
-    return {
-        initData: function(inputId, chartId){
-            document.querySelector("#"+inputId).addEventListener(
+}
+
+function _heredoc(fn){
+    return fn.toString().split('\n').slice(1,-1).join('\n') + '\n'
+}
+
+export let initData = function(inputId, chartId){
+    document.querySelector("#"+inputId).addEventListener(
             "change", function(event){
                 _init(event.target, chartId)
             })            
-        },
-        configBtn: function(chartId, btnId, type){
-            document.querySelector("#"+btnId).addEventListener(
+}
+
+export let configBtn = function(chartId, btnId, type){
+    document.querySelector("#"+btnId).addEventListener(
             "mouseenter", function(){
                 _createOnEvent(type, chartId)
             })
-        },
-        createChartUseVar: function(chartId, varCsv, type){
-            _set(_parseString(varCsv))
-            _draw(chartId, _get(), type)
-        },
-        heredoc: function(fn) {
-            return _heredoc(fn)
-        }
-    }
-})
+}
+
+export let createChartUseVar = function(chartId, varCsv, type){
+    _set(_parseString(varCsv))
+        _draw(chartId, _get(), type)
+}
+
+export let heredoc = function(fn) {
+    return _heredoc(fn)
+}
